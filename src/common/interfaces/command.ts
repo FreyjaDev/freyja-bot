9698c6545c6cb737cce1e538d40786d55b0c77e4
Lazialize <lazinelia@gmail.com>
2024-01-21 15:26:02 +0900

    chore: add TODO

--- src/common/interfaces/command.ts
+++ src/common/interfaces/command.ts
@@ -4,6 +4,7 @@ import {
 	SlashCommandSubcommandsOnlyBuilder,
 } from "discord.js";
 
+// TODO: Add support for subcommand groups to the executor
 export interface Command {
 	commandBuilder: SlashCommandBuilder | SlashCommandSubcommandsOnlyBuilder;
 	executor: (interaction: CommandInteraction) => Promise<void>;
